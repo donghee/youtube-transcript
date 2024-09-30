9ec8c1e0d1ba95b098eaa08095a3edc42e8acc2e
Donghee Park <dongheepark@gmail.com>
2024-10-01 07:51:40 +0900

    refactor and fix error for long video

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from trans_youtube import youtube_transcript, youtube_title2
+from trans_youtube import generate_transcript_page, youtube_title2
 from flask import Flask, render_template, request, redirect, url_for
 from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED, EVENT_JOB_ADDED
 from apscheduler.schedulers.background import BackgroundScheduler
 import os
+
+static_folder = 'output'
+app = Flask(__name__, static_folder=static_folder)
 
 jobs_status = {}
 scheduler = BackgroundScheduler()
@@ -10,23 +13,27 @@ scheduler.start()
 
 def scheduler_listener(event):
     if event.code == EVENT_JOB_EXECUTED:
-        print('The youtube trascript job excuted :)')
-        jobs_status.pop(event.job_id)
+        print('The youtube transcript job executed :)')
+        url = event.job_id
+        jobs_status.pop(url)
+        #  socketio.emit('job_completed', {'job_id': job_id})
     elif event.code == EVENT_JOB_ADDED:
-        print('The youtube trascript job was added')
+        print('The youtube transcript job was added')
+    elif event.code == EVENT_JOB_ERROR:
+        print('The youtube transcript job failed :(')
+        url = event.job_id
+        jobs_status[url]['status'] = 'Failed'
+        jobs_status[url]['msg'] = 'Because of an Claude API Error. Please try again'
 
 scheduler.add_listener(scheduler_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR | EVENT_JOB_ADDED)
-
-static_folder = 'output'
-app = Flask(__name__, static_folder=static_folder)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         url = request.form['url']
         #  youtube_transcript(url, audio_only=False)
-        job = scheduler.add_job(func=youtube_transcript, args=(url,), kwargs={'audio_only': False}, id=url)
-        jobs_status[url] = {'status': 'Processing...', 'args': url}
+        job = scheduler.add_job(func=generate_transcript_page, args=(url,), kwargs={'audio_only': False}, id=url)
+        jobs_status[url] = {'status': 'Processing...', 'args': url, 'msg': ''}
         return redirect(url_for('index'))
     
     video_dirs = [d for d in os.listdir(static_folder) if os.path.isdir(os.path.join(static_folder, d))]
@@ -38,7 +45,7 @@ def index():
         videos.append({'image': f'https://img.youtube.com/vi/{video_id}/maxresdefault.jpg', 'title': title,
                       'url': f'/{static_folder}/{video_id}/index.html', 'transcript': transcript})
 
-    jobs_status_ = ', '.join([f'{v["status"]} {v["args"]}' for k, v in jobs_status.items()])
+    jobs_status_ = ', '.join([f'{v["status"]} {v["msg"]} {v["args"]}' for k, v in jobs_status.items()])
 
     return render_template('index.html', videos=videos, jobs_status=jobs_status_)
 
--- trans_youtube.py
+++ trans_youtube.py
@@ -7,22 +7,11 @@ import argparse
 import assemblyai as aai
 import os
 import yt_dlp
+from tqdm import tqdm
 
 #  save_path = "/tmp"
 current_path = os.path.dirname(os.path.realpath(__file__))
 save_path = f"{current_path}/output"
-
-audio_only_ydl_opts = {
-    'format': 'm4a/bestaudio/best',  # The best audio version in m4a format
-    'outtmpl': '%(id)s.%(ext)s',  # The output name should be the id followed by the extension
-    'postprocessors': [{  # Extract audio using ffmpeg
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'm4a',
-    }],
-    'quiet': True,  # Suppress output
-    'paths': {"home": save_path},  # Save the file to save_path
-    'writesubtitles': True
-}
 
 def chunk_text_to_paragraphs_semantic(text):
     # NLTK의 TextTilingTokenizer를 사용하여 텍스트를 의미 단위로 분할
@@ -59,30 +48,46 @@ def translate_vtt(vtt):
     client = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
 
     # TODO: optimize best request line size
-    request_line_size = 65
+    line_size_one_request = int(66/3)
     vtts = []
-    lines = vtt.split("\n")
-    for i in range(0, len(lines), request_line_size):
-        vtts.append("\n".join(lines[i:i+request_line_size]))
+    lines = vtt.split("\n\n")
+    for i in range(0, len(lines), line_size_one_request):
+        vtts.append("\n\n".join(lines[i:i+line_size_one_request]))
 
     translated_vtts = []
-    for vtt in vtts:
-        response = client.messages.create(
-            model="claude-3-5-sonnet-20240620",
-            max_tokens=1024,
-            system="당신은 유능한 AI 한국어 번역 어시스턴트입니다. vtt 자막 파일을 입력 받아서, 사용자의 질문에 대해 확인 없이 바로 간결하고 정확한 한국어 vtt 자막 파일을 제공하세요. 불필요한 인사말이나 설명은 생략하고 핵심 정보만 전달하세요.",
-            messages=[{"role": "user", "content": f"{vtt}"}]
-        )
-        content = response.content[0].text
-        translated_vtts.append(content)
+    for vtt in tqdm(vtts, desc="Translating VTT"):
+        max_retries = 3
+        for attempt in range(max_retries):
+            try:
+                response = client.messages.create(
+                    model="claude-3-5-sonnet-20240620",
+                    max_tokens=1024,
+                    system="당신은 유능한 AI 한국어 번역 어시스턴트입니다. vtt 자막 파일을 입력 받아서, 사용자의 질문에 대해 확인 없이 바로 간결하고 정확한 한국어 vtt 자막 파일을 제공하세요. 불필요한 인사말이나 설명은 생략하고 핵심 정보만 전달하세요.",
+                    messages=[{"role": "user", "content": f"{vtt}"}]
+                )
+                content = response.content[0].text
+                translated_vtts.append(content)
+                break
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                if attempt < max_retries - 1:
+                    print(f"Retrying... (Attempt {attempt + 2}/{max_retries})")
+                else:
+                    print(f"Failed to translate after {max_retries} attempts. Skipping this chunk.")
+                    translated_vtts.append(vtt)  # 번역 실패 시 원본 텍스트 추가
 
     return translated_vtts
 
-def youtube_title(video_id):
+def youtube_info(URL):
     ydl_opts = { 'quiet': True }
+    title = ""
+    vid = ""
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        title = ydl.extract_info(video_id, download=False).get('title', None)
-    return title
+        vid = ydl.extract_info(URL, download=False).get('id', None)
+        title = ydl.extract_info(vid, download=False).get('title', None)
+
+    return vid, title
+
 
 def youtube_title2(video_id):
     transcript = os.path.join(save_path, video_id, f'{video_id}_transcript.txt')
@@ -92,81 +97,115 @@ def youtube_title2(video_id):
             return f.readline().split(' - ')[0]
     return ""
 
-def youtube_transcript(URL, audio_only=False):
+def youtube_transcript(URL):
     paragraphs = []
+    transcript = None
+    audio_only_ydl_opts = {
+        'format': 'm4a/bestaudio/best',  # The best audio version in m4a format
+        'outtmpl': '%(id)s.%(ext)s',  # The output name should be the id followed by the extension
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+        }],
+        'quiet': True,  # Suppress output
+        'paths': {"home": save_path},  # Save the file to save_path
+        'writesubtitles': True
+    }
 
-    ydl_opts = { 'outtmpl': '%(id)s.%(ext)s', 'paths': {"home": save_path}, 'writesubtitles': True }
-
-    if audio_only:
-        ydl_opts = audio_only_ydl_opts
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL(audio_only_ydl_opts) as ydl:
         error_code = ydl.download(URL)
-        title = ydl.extract_info(URL, download=False).get('title', None)
-        vid = ydl.extract_info(URL, download=False).get('id', None)
+        vid, title = youtube_info(URL)
         filename = ydl.prepare_filename(ydl.extract_info(URL, download=False))
+        filesize = os.path.getsize(filename) / 1024 / 1024
+        print(f"Download: {filename}, {filesize} MB")
 
         # transcribe the audio
         config = aai.TranscriptionConfig(language_detection=True)
         transcriber = aai.Transcriber(config=config)
         transcript = transcriber.transcribe(filename)
+
+        if transcript.status == aai.TranscriptStatus.error:
+            print(f"Transcription failed: {transcript.error}")
+            raise Exception(f"Transcription failed: {transcript.error}")
     
         print("Original transcript:")
         print(transcript.text)
         print("---")
+
+    return transcript
+
+def save_transcript(transcript, vid, title):
+    # save the transcript to a file
+    transcript_filename = f"{save_path}/{vid}_transcript.txt"
+    with open(transcript_filename, "w") as f:
+        f.write(f"{title} - https://www.youtube.com/watch?v={vid}\n\n")
+        f.write(transcript.text)
+
+    # save the transcript to a vtt file
+    vtt = transcript.export_subtitles_vtt()
+    en_vtt_filename = f"{save_path}/{vid}.en.vtt"
+    with open(en_vtt_filename, "w") as f:
+        f.write(vtt)
+
+    ko_vtts = translate_vtt(vtt)
+    print("\nKorean translation:")
+    print(len(ko_vtts))
+    ko_vtt_filename = f"{save_path}/{vid}.ko.vtt"
+    with open(ko_vtt_filename, "w") as f:
+        f.write("\n".join(ko_vtts))
+
+    # copy the transcript to the clipboard
+    #  if os.system("which /usr/bin/xclip > /dev/null 2>&1") == 0: # linux
+    #      print("Transcript copied to clipboard")
+    #      os.system(f"xclip -selection clipboard {save_path}/{vid}_transcript.txt")
+    #
+    #  if os.system("which /usr/bin/pbcopy > /dev/null 2>&1") == 0: # mac os
+    #      print("Transcript copied to clipboard")
+    #      os.system(f"pbcopy < {save_path}/{vid}_transcript.txt")
     
-        # save the transcript to a file
-        with open(f"{save_path}/{vid}_transcript.txt", "w") as f:
-            f.write(f"{title} - {URL}\n\n")
-            f.write(transcript.text)
+    #  paragraphs = chunk_text_to_paragraphs_semantic(transcript.text)
+    # print("\nParagraphs:")
+    # print(paragraphs)
+    return transcript_filename, en_vtt_filename, ko_vtt_filename
 
-        # save the transcript to a vtt file
-        vtt = transcript.export_subtitles_vtt()
-        en_vtt_filename = f"{filename}.en.vtt"
-        with open(en_vtt_filename, "w") as f:
-            f.write(vtt)
+def generate_html(vid, title, transcript_filename, en_vtt, ko_vtt):
+    video_src = f"{vid}.mp4"
+    en_vtt_src = os.path.basename(en_vtt)
+    ko_vtt_src = os.path.basename(ko_vtt)
 
-        ko_vtts = translate_vtt(vtt)
-        print("\nKorean translation:")
-        print(len(ko_vtts))
-        ko_vtt_filename = f"{filename}.ko.vtt"
-        with open(ko_vtt_filename, "w") as f:
-            f.write("\n".join(ko_vtts))
+    transcript = ""
+    with open(transcript_filename, "r") as f:
+        transcript = f.read()
 
-        # copy the transcript to the clipboard
-        if os.system("which /usr/bin/xclip > /dev/null 2>&1") == 0: # linux
-            print("Transcript copied to clipboard")
-            os.system(f"xclip -selection clipboard {save_path}/{vid}_transcript.txt")
-    
-        if os.system("which /usr/bin/pbcopy > /dev/null 2>&1") == 0: # mac os
-            print("Transcript copied to clipboard")
-            os.system(f"pbcopy < {save_path}/{vid}_transcript.txt")
-    
-        paragraphs = chunk_text_to_paragraphs_semantic(transcript.text)
-        # print("\nParagraphs:")
-        # print(paragraphs)
+    write_video_html(title, video_src, en_vtt_src, ko_vtt_src, transcript)
 
-        video_src = os.path.basename(filename)
-        en_vtt_src = os.path.basename(en_vtt_filename)
-        ko_vtt_src = os.path.basename(ko_vtt_filename)
-        if not audio_only:
-            write_video_html(title, video_src, en_vtt_src, ko_vtt_src)
-            os.system(f"mkdir -p {save_path}/{vid}")
-            os.system(f"cp {save_path}/{vid}* {save_path}/{vid}")
-            os.system(f"cp {save_path}/index.html {save_path}/{vid}")
+    os.system(f"mkdir -p {save_path}/{vid}")
+    os.system(f"cp {save_path}/{vid}* {save_path}/{vid}")
+    os.system(f"cp {save_path}/index.html {save_path}/{vid}")
+
+def save_video(URL): 
+    ydl_opts = { 'outtmpl': '%(id)s.%(ext)s', 'paths': {"home": save_path}, 'writesubtitles': True }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download(URL)
+        vid, title = youtube_info(URL)
+        filename = ydl.prepare_filename(ydl.extract_info(URL, download=False))
+        filesize = os.path.getsize(filename) / 1024 / 1024
+        print(f"Download: {filename}, {filesize} MB")
 
         # for ios playable video, convert to mp4
         base_filename = filename.split(".")[0]
-        video_converting_cmd = f"ffmpeg -y -hide_banner -i {filename} -c:v libx264 -preset veryfast -crf 22 -movflags +faststart -c:a aac -b:a 192k -pix_fmt yuv420p {base_filename}_converted.mp4"
-        os.system(video_converting_cmd)
-        os.system(f"mv {base_filename}_converted.mp4 {save_path}/{vid}/{video_src}")
+        video_filter=""
+        if filesize > 1000: # 1000MB
+            video_filter="-vf scale=1920:-1" # 1080p
 
-        print(f"Transcript saved to {save_path}/{vid}_transcript.txt")
+        video_converting_cmd = f"ffmpeg -y -hide_banner -i {filename} {video_filter} -c:v libx264 -preset veryfast -crf 22 -movflags +faststart -c:a aac -b:a 192k -pix_fmt yuv420p {base_filename}_converted.mp4"
+        os.system(video_converting_cmd)
+        os.system(f"mv {base_filename}_converted.mp4 {save_path}/{vid}/{vid}.mp4")
+
         print(f"Video saved to {save_path}/{vid}")
 
-    return paragraphs
-
-def write_video_html(title, video_src, en_vtt_src, ko_vtt_src):
+def write_video_html(title, video_src, en_vtt_src, ko_vtt_src, transcript):
     # download from youtube
     original_vtt_src = video_src.replace("mp4", "en")+ ".vtt"
 
@@ -203,6 +242,19 @@ def write_video_html(title, video_src, en_vtt_src, ko_vtt_src):
                 <track kind="subtitles" src="{ko_vtt_src}" srclang="ko" label="한국어" default>
             </video>
         </div>
+        <div class="mt-10 max-w-3xl mx-auto">
+            <p>
+            From: https://www.youtube.com/watch?v={video_src.split('.')[0]}
+            </p>
+        </div>
+        <div class="mt-10 max-w-3xl mx-auto">
+            <h2 class="text-2xl font-bold mb-4">Transcript</h2>
+            <div class="bg-white p-4 rounded-lg shadow-md">
+               <pre class="whitespace-pre-wrap">
+{transcript}
+               </pre>
+            </div>
+        </div>
     </div>
 </body>
 </html>
@@ -210,9 +262,16 @@ def write_video_html(title, video_src, en_vtt_src, ko_vtt_src):
     with open(f"{save_path}/index.html", "w") as f:
         f.write(html)
 
+def generate_transcript_page(URL, audio_only=False):
+    transcript = youtube_transcript(URL)
+    vid, title = youtube_info(URL)
+    transcript_filename, en_vtt_filename, ko_vtt_filename = save_transcript(transcript, vid, title)
+    generate_html(vid, title, transcript_filename, en_vtt_filename, ko_vtt_filename)
+    save_video(URL)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Transcribe and translate a YouTube video')
     parser.add_argument('URL', type=str, help='The URL of the YouTube video to transcribe and translate')
     args = parser.parse_args()
-    paragraphs = youtube_transcript(args.URL, audio_only=False)
-    # translate(paragraphs)
+
+    generate_transcript_page(args.URL)
